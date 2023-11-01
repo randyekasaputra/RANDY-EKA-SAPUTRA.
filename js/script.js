53c8b222837f0edebe30e3914cea31cd9a83a152
randyekasaputra <149234797+randyekasaputra@users.noreply.github.com>
2023-11-01 15:03:35 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ document.querySelector("#tugas-menu");
 onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// Klik di luar sidebar untuk menghilangkan nan
+const tugas = document.querySelector("#tugas-menu");
+
+document.addEventListener("click", function (e) {
+  if (!tugas.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
